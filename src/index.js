c1b24f43d76e7d280d92f8974d2497005874124f
Amy Jung <ajwjung@outlook.com>
2023-01-14 13:42:27 -0800

    refactor: Separate default load code to module

--- src/index.js
+++ src/index.js
@@ -1,80 +1,9 @@
 import { FormFields } from "./modules/form-handler.js";
 import { DomElements } from "./modules/dom-elements.js";
 import { SidebarHandler } from "./modules/sidebar-handler.js";
-import { TaskDom, TaskRemoval } from "./modules/task-handler.js";
-import { CategoryDom } from "./modules/category-handler.js";
-import { DataArr } from "./modules/storage-arrays.js";
+import { TaskRemoval } from "./modules/task-handler.js";
 import { TaskEditor } from "./modules/task-editor.js";
-import { CategoryStorage, TaskStorage } from "./modules/local-storage.js";
-
-const DefaultLoad = (() => {
-    const setDefault = () => {
-        // If local storage not set up yet
-        if (!(CategoryStorage.getCategories() && TaskStorage.getTasks())) {
-            CategoryStorage.setCategories();
-            TaskStorage.setTasks();
-            SidebarHandler.setInitialCategory();
-            setDefaultActive();
-        };
-    };
-
-    const loadSavedCategories = () => {
-        const allCategories = CategoryStorage.getCategories();
-        if (allCategories && allCategories.length > 1) {
-            for (let i = 1; i < allCategories.length; i++) {
-                CategoryDom.appendCategoryDiv(allCategories[i]);
-                DomElements.addExistingOptions(allCategories[i]);
-            };
-            setOtherActive();
-        };
-    };
-
-    const loadSavedTasks = () => {
-        TaskDom.createAllTaskDivs();
-    };
-
-    const addNewCategory = () => {
-        const projectForm = document.querySelector("#projects-form");
-        projectForm.addEventListener("submit", function(e) {
-            e.preventDefault();
-            if (DataArr.checkNewCategoryAdded(FormFields.getCategoryName())) {
-                FormFields.newCategoryHandler(); // append to array
-                CategoryDom.appendCategoryDiv(CategoryDom.getNewCategory()); // add dom element
-                DomElements.addNewOptionToSelect(CategoryDom.getNewCategory()); 
-                projectForm.reset();
-                DataArr.resetCategoryAdded();
-            }
-        });
-    };
-    
-    const addNewTask = () => {        
-        const taskForm = document.querySelector("#task-form");
-        taskForm.addEventListener("submit", function(e) {
-            e.preventDefault();
-            FormFields.newTaskHandler(e);
-            const currentTab = SidebarHandler.getTabName();
-            const newTask = FormFields.getTaskObject("new task");
-            if (newTask.category == currentTab) {
-                TaskDom.appendTaskDiv(newTask);
-            };
-            taskForm.reset();
-        });
-    };
-
-    const setDefaultActive = () => {
-        const defaultTab = document.getElementById("default");
-        SidebarHandler.setThisDivActive(defaultTab);
-    };
-
-    const setOtherActive = () => {
-        SidebarHandler.resetActiveDiv();
-        const lastOpenedTab = document.getElementById(SidebarHandler.getTabName());
-        SidebarHandler.setThisDivActive(lastOpenedTab);
-    };
-
-    return { setDefault, loadSavedCategories, loadSavedTasks, 
-        addNewCategory, addNewTask, setDefaultActive };
-})();
+import { DefaultLoad } from "./modules/default-load.js";
 
 const RenderPage = (() => {
     FormFields.setMinDatePicker("#task-due-date");
@@ -94,5 +23,4 @@ const RenderPage = (() => {
     SidebarHandler.tabHandler();
     TaskRemoval.removeTaskHandler();
     TaskEditor.editTaskHandler();
-
 })();
---
+++ src/modules/default-load.js
@@ -0,0 +1,78 @@
+import { FormFields } from "./form-handler.js";
+import { DomElements } from "./dom-elements.js";
+import { SidebarHandler } from "./sidebar-handler.js";
+import { TaskDom } from "./task-handler.js";
+import { CategoryDom } from "./category-handler.js";
+import { DataArr } from "./storage-arrays.js";
+import { CategoryStorage, TaskStorage } from "./local-storage.js";
+
+const DefaultLoad = (() => {
+    const setDefault = () => {
+        // If local storage not set up yet
+        if (!(CategoryStorage.getCategories() && TaskStorage.getTasks())) {
+            CategoryStorage.setCategories();
+            TaskStorage.setTasks();
+            SidebarHandler.setInitialCategory();
+            setDefaultActive();
+        };
+    };
+
+    const loadSavedCategories = () => {
+        const allCategories = CategoryStorage.getCategories();
+        if (allCategories && allCategories.length > 1) {
+            for (let i = 1; i < allCategories.length; i++) {
+                CategoryDom.appendCategoryDiv(allCategories[i]);
+                DomElements.addExistingOptions(allCategories[i]);
+            };
+            setOtherActive();
+        };
+    };
+
+    const loadSavedTasks = () => {
+        TaskDom.createAllTaskDivs();
+    };
+
+    const addNewCategory = () => {
+        const projectForm = document.querySelector("#projects-form");
+        projectForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+            if (DataArr.checkNewCategoryAdded(FormFields.getCategoryName())) {
+                FormFields.newCategoryHandler(); // append to array
+                CategoryDom.appendCategoryDiv(CategoryDom.getNewCategory());
+                DomElements.addNewOptionToSelect(CategoryDom.getNewCategory()); 
+                projectForm.reset();
+                DataArr.resetCategoryAdded();
+            }
+        });
+    };
+    
+    const addNewTask = () => {        
+        const taskForm = document.querySelector("#task-form");
+        taskForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+            FormFields.newTaskHandler(e);
+            const currentTab = SidebarHandler.getTabName();
+            const newTask = FormFields.getTaskObject("new task");
+            if (newTask.category == currentTab) {
+                TaskDom.appendTaskDiv(newTask);
+            };
+            taskForm.reset();
+        });
+    };
+
+    const setDefaultActive = () => {
+        const defaultTab = document.getElementById("default");
+        SidebarHandler.setThisDivActive(defaultTab);
+    };
+
+    const setOtherActive = () => {
+        SidebarHandler.resetActiveDiv();
+        const lastOpenedTab = document.getElementById(SidebarHandler.getTabName());
+        SidebarHandler.setThisDivActive(lastOpenedTab);
+    };
+
+    return { setDefault, loadSavedCategories, loadSavedTasks, 
+        addNewCategory, addNewTask, setDefaultActive };
+})();
+
+export { DefaultLoad };
